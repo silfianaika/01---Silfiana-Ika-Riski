eee8031cc473bb923878393e627b73e7223debaa
silfianaika <silfianaika@gmail.com>
2023-11-15 14:26:09 +0700

    first commit

---
+++ application/views/footer.php
@@ -0,0 +1,44 @@
+</div><!-- container -->
+
+</div> <!-- Page content Wrapper -->
+
+</div> <!-- content -->
+
+<footer class="footer">
+    LSP - Silfiana Ika Riski - 2131730076
+</footer>
+
+</div>
+<!-- End Right content here -->
+
+</div>
+<!-- END wrapper -->
+
+
+<!-- jQuery  -->
+<script src="<?= base_url() ?>assets/js/jquery.min.js"></script>
+<script src="<?= base_url() ?>assets/js/popper.min.js"></script>
+<script src="<?= base_url() ?>assets/js/bootstrap.min.js"></script>
+<script src="<?= base_url() ?>assets/js/modernizr.min.js"></script>
+<script src="<?= base_url() ?>assets/js/detect.js"></script>
+<script src="<?= base_url() ?>assets/js/fastclick.js"></script>
+<script src="<?= base_url() ?>assets/js/jquery.slimscroll.js"></script>
+<script src="<?= base_url() ?>assets/js/jquery.blockUI.js"></script>
+<script src="<?= base_url() ?>assets/js/waves.js"></script>
+<script src="<?= base_url() ?>assets/js/jquery.nicescroll.js"></script>
+<script src="<?= base_url() ?>assets/js/jquery.scrollTo.min.js"></script>
+
+<!-- Required datatable js -->
+<script src="<?= base_url() ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url() ?>assets/plugins/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="<?= base_url() ?>assets/plugins/select2/select2.min.js" type="text/javascript"></script>
+
+<script src="<?= base_url() ?>assets/plugins/alertify/js/alertify.js"></script>
+<script src="<?= base_url() ?>assets/pages/alertify-init.js"></script>
+
+<!-- App js -->
+<script src="<?= base_url() ?>assets/js/app.js"></script>
+
+</body>
+
+</html>
