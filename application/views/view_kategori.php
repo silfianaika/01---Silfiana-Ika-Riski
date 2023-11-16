4478903ef6dd98ef713c2d55b7f39351ba269f5a
silfianaika <149127226+silfianaika@users.noreply.github.com>
2023-11-16 22:27:22 +0700

    Update view_kategori.php

--- application/views/view_kategori.php
+++ application/views/view_kategori.php
@@ -44,7 +44,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title mt-0" id="myLargeModalLabel">Tambah kategori surat</h5>
+                <h5 class="modal-title mt-0" id="myLargeModalLabel">Kategori Surat >> Tambah</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
             </div>
             <div class="modal-body">
